fec239ea5af4292077d851bc8905b8f72f46c33c
Saiki <i@baka.live>
2022-11-07 11:55:58 +0800

    DO WHAT THE FUCK YOU WANT

---
+++ sh/6969.sh
@@ -0,0 +1,20 @@
+rm -rf ngrok ngrok.zip ngrok.sh > /dev/null 2>&1
+wget -O ng.sh https://raw.githubusercontent.com/5t-RawBeRry/Sorry-Colab/main/sh/ng.sh > /dev/null 2>&1
+chmod +x ng.sh
+./ng.sh
+clear
+echo "======================="
+echo choose ngrok region
+echo "======================="
+echo "us - United States (Ohio)"
+echo "eu - Europe (Frankfurt)"
+echo "ap - Asia/Pacific (Singapore)"
+echo "au - Australia (Sydney)"
+echo "sa - South America (Sao Paulo)"
+echo "jp - Japan (Tokyo)"
+echo "in - India (Mumbai)"
+read -p "choose ngrok region: " CRP
+./ngrok tcp --region $CRP 6969 &>/dev/null &
+echo "===================================="
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo "===================================="
